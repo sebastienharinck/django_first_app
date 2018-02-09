d84de2da091ac295464e1cea329679cad4c8ee9c
seb <harinck.sebastien@gmail.com>
2018-02-09 14:49:32 +0100

    Question Model rename pub_data into pub_date

--- polls/models.py
+++ polls/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_data = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
