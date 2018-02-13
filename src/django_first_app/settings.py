6503326c0f305b20e9b1c780ba2718b23d94d978
seb <harinck.sebastien@gmail.com>
2018-02-13 19:33:20 +0100

    create the app odaxiom_dashboard for refactor

--- src/django_first_app/settings.py
+++ src/django_first_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'odaxiom_dashboard.apps.OdaxiomDashboardConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ src/odaxiom_dashboard/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class OdaxiomDashboardConfig(AppConfig):
+    name = 'odaxiom_dashboard'
